e62720ab95afe23b10055f3acd71b34c54abd31a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-09-13 19:50:17 -0400

    added rack-cors gem and configured its initialzier. still need to add frontend url to initializer

--- app/controllers/v1/topics_controller.rb
+++ app/controllers/v1/topics_controller.rb
@@ -1,2 +1,15 @@
 class V1::TopicsController < ApplicationController
+	def index
+		render json: Topic.all, each_serializer: V1::TopicSerializer
+	end
+
+	def create
+		@topic = Topic.create(topic_params)
+	end
+
+	private
+
+		def topic_params
+			params.require(:topic).permit(:title)
+		end
 end
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,2 +1,27 @@
 class V1::UsersController < ApplicationController
+	def index
+		# use each_serializer for a serialized colleciton
+		render json: User.all, each_serializer: V1::UserSerializer
+	end
+
+	def create
+		@user = User.create(user_params)
+	end
+
+	def show
+		@user = User.find(params[:id])
+		render json: @user
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :password)
+		end
 end
